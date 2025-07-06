960a69db4a74186d6bf251ba71be1ee56bd0347b
skykwon1004 <skykwon1004@gmail.com>
2025-07-06 16:00:38 +0900

    리액트 에러

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import React from "react";
 import { IoIosClose, IoIosLink, IoLogoGithub } from "react-icons/io";
 import useDarkModeStore from "../zustand/useDarkModeStore";
 
